b2357173f9de0c49c0dfb27d8e5cbb3fcf208488
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-11-23 18:47:27 +0900

    1st commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { setupStore } from "~/store/store";
+import { AuthProvider } from "~/context/Auth";
+
+import "../styles/tailwind.css";
+import "../styles/index.scss";
+
+const store = setupStore();
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
+};
+
+export default App;
