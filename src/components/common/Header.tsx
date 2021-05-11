9d56e5f1a4d86b0b4e2ff21f22d9dacf83ec4dfd
yujiokayama <y.okayama.13@gmail.com>
2021-05-12 00:06:20 +0900

    update

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,25 +2,27 @@ import Link from "next/link";
 import Image from "next/image";
 import TagList from "~/components/tag-list";
 
+const Logo = () => {
+  return (
+    <Image
+      src="/assets/images/logo.svg"
+      alt="YS-NOTE"
+      width={100}
+      height="auto"
+      priority
+    />
+  );
+};
+
 const Header = () => {
-  const Logo = () => {
-    return (
-      <Image
-        src="/assets/images/logo.svg"
-        alt="YS-NOTE"
-        width={100}
-        height="auto"
-        priority
-      />
-    );
-  };
   return (
     <>
       <header>
         <h1 className="logo">
           <Link href="/">
             <a>
-              <Logo />
+              {/* <Logo /> */}
+              <img src="/assets/images/logo.svg" width="100" alt="" />
             </a>
           </Link>
         </h1>
